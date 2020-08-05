6b0d6ccd8a54eb13fe4cc5754926dd062986c75e
Zhenghua Lyu <kainwen@gmail.com>
2020-08-05 09:24:47 +0800

    more search

--- software_tools/search.erl
+++ software_tools/search.erl
@@ -1,6 +1,6 @@
 -module(search).
 
--export([dfs/3, bfs/3]).
+-export([dfs/3, bfs/3, best_first_search/4, beam_search/5]).
 
 tree_search([], _DoWeFinish, _Successors, _Combiner) -> fail;
 tree_search([S|Rest], DoWeFinish, Successors, Combiner) ->
@@ -28,4 +28,25 @@ bfs(State, DoWeFinish, Successors) ->
 		Successors,
 		fun (Succ, Rest) ->
 			Rest ++ Succ
+		end).
+
+sorter(CostFn) ->
+    fun (Succ, Rest) ->
+	    lists:sort(fun (A, B) ->
+			       CostFn(A) < CostFn(B)
+		       end,
+		       Succ ++ Rest)
+    end.
+
+best_first_search(State, DoWeFinish, Successors, CostFn) ->
+    tree_search([State], DoWeFinish, Successors, sorter(CostFn)).
+
+beam_search(State, DoWeFinish, Successors, CostFn, BeamWidth) ->
+    tree_search([State], DoWeFinish, Successors,
+		fun (Succ, Rest) ->
+			Sorted = lists:sort(fun (A, B) ->
+						    CostFn(A) < CostFn(B)
+					    end,
+					    Succ ++ Rest),
+			lists:sublist(Sorted, BeamWidth)
 		end).
